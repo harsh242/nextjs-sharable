6f356d42101f3e234b9c3d6fd8a003bafd6813e5
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2021-04-05 16:20:45 +0530

    add dynamic data

--- pages/tezos/cryptobot/index.js
+++ pages/tezos/cryptobot/index.js
@@ -36,7 +36,7 @@ export default function Cryptobot() {
       <Head>
         <title>Cryptobot #{bot.tokenId}</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="title" content="Meta Tags — Preview, Edit and Generate" />
+        <meta name="title" content={`Cryptobot #${bot.tokenId}`} />
         <meta
           name="description"
           content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
@@ -44,33 +44,21 @@ export default function Cryptobot() {
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://metatags.io/" />
-        <meta
-          property="og:title"
-          content="Meta Tags — Preview, Edit and Generate"
-        />
+        <meta property="og:title" content={`Cryptobot #${bot.tokenId}`} />
         <meta
           property="og:description"
           content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
         />
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="og:image" content={base64.value} />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://metatags.io/" />
-        <meta
-          property="twitter:title"
-          content="Meta Tags — Preview, Edit and Generate"
-        />
+        <meta property="twitter:title" content={`Cryptobot #${bot.tokenId}`} />
         <meta
           property="twitter:description"
           content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
         />
-        <meta
-          property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="twitter:image" content={base64.value} />
       </Head>
 
       <main className={styles.main}>
