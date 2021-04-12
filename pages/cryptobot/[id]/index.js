8826a53cd52a4691c55afee4497f667b10369a17
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2021-04-12 15:06:02 +0530

    :sparkles: dynamic seo

---
+++ pages/cryptobot/[id]/index.js
@@ -0,0 +1,79 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../../../styles/Home.module.css";
+import Head from "next/head";
+
+const Cryptobot = ({ id, image }) => {
+  return (
+    <>
+      <Head>
+        <title>Cryptobot #{id}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="title" content={`Cryptobot #${id}`} />
+        <meta
+          name="description"
+          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content={`Cryptobot #${id}`} />
+        <meta
+          property="og:description"
+          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+        />
+        <meta property="og:image" content={image} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content={`Cryptobot #${id}`} />
+        <meta
+          property="twitter:description"
+          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+        />
+        <meta property="twitter:image" content={image} />
+      </Head>
+      <h1>{id}</h1>
+      <img src={image} className={styles.image} />
+    </>
+  );
+};
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://cryptoverse-wars-backend-pr-16.onrender.com/cryptobot?id=${context.params.id}`
+  );
+
+  const bot = await res.json();
+  return {
+    props: {
+      id: bot.token_id,
+      image: sanitizeJsonUri(bot.imageURI),
+    },
+  };
+};
+
+function sanitizeJsonUri(origin) {
+  if (origin.startsWith("ipfs://")) {
+    return `https://cloudflare-ipfs.com/ipfs/${origin.substring(7)}/`;
+  }
+  return null;
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(
+    `https://cryptoverse-wars-backend-pr-16.onrender.com/cryptobot/all`
+  );
+
+  const bots = await res.json();
+
+  const ids = bots.map((bot) => bot.token_id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export default Cryptobot;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,18 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import React, { useMemo, useState, useEffect } from "react";
-import { fetchAllNfts } from "../lib/indexer";
-import { useAsync } from "react-use";
-import Link from "next/link";
-import HomeBot from "../component/Bot";
 
-export default function Home({ combined }) {
-  const [nftList, updateNftList] = useState([]);
-  console.log("combined inside fn", combined);
-  const allNFTS = useAsync(async () => {
-    try {
-      const combined = await fetchAllNfts();
-      console.log("combined", combined);
-      updateNftList(combined);
-      return combined;
-    } catch (e) {
-      console.log(e);
-    }
-  }, []);
-
-  useEffect(() => {
-    //get all the nft tokens
-    const all = allNFTS.value ? allNFTS.value : [];
-  }, []);
-
-  // useEffect(() => {
-  //   const all = combined ? combined : [];
-  //   updateNftList(all);
-  // }, []);
+export default function Home({}) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>share.cryptocodeschool.in</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Loading... </h1>
-        <div className={styles.grid}>
-          {nftList.length > 0 &&
-            nftList.map((el) => {
-              return (
-                <div key={el.tokenId} className={styles.card}>
-                  <HomeBot botInfo={el} />
-                </div>
-              );
-            })}
-        </div>
+        <h1 className={styles.title}>Redirecting </h1>
       </main>
     </div>
   );
 }
-
-// export const getStaticProps = async () => {
-//   const combined = await fetchAllNfts();
-//   console.log("combined inside get props", combined);
-
-//   return {
-//     props: {
-//       json,
-//     },
-//   };
-// };
