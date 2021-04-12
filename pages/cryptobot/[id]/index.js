19802a5ba6849fcd5e022df2597bcf12df9b2da6
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2021-04-12 17:37:41 +0530

    :sparkles: sharable link ready to test

--- pages/cryptobot/[id]/index.js
+++ pages/cryptobot/[id]/index.js
@@ -1,47 +1,62 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import styles from "../../../styles/Home.module.css";
 import Head from "next/head";
 
 const Cryptobot = ({ id, image }) => {
-  useEffect(() => {
-    window.location.href = `https://cryptocodeschool.in/tezos/cryptobot/${id}`;
-  }, []);
+  //   useEffect(() => {
+  //     window.location.href = `https://cryptocodeschool.in/tezos/cryptobot/${id}`;
+  //   }, []);
   return (
     <>
       <Head>
         <title>Cryptobot #{id}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.svg" />
         <meta name="title" content={`Cryptobot #${id}`} />
         <meta
           name="description"
-          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+          content="Take a look at this awesome Cryptobot NFT ⚡️. Grab your unique Cryptobot today!"
         />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:url" content="https://cryptocodeschool.in/tezos" />
         <meta property="og:title" content={`Cryptobot #${id}`} />
         <meta
           property="og:description"
-          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+          content="Take a look at this awesome Cryptobot NFT ⚡️. Grab your unique Cryptobot today!"
         />
         <meta property="og:image" content={image} />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta
+          property="twitter:url"
+          content="https://cryptocodeschool.in/tezos"
+        />
         <meta property="twitter:title" content={`Cryptobot #${id}`} />
         <meta
           property="twitter:description"
-          content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+          content="Take a look at this awesome Cryptobot NFT ⚡️. Grab your unique Cryptobot today!"
         />
         <meta property="twitter:image" content={image} />
       </Head>
-      <h1>{id}</h1>
-      <img src={image} className={styles.image} />
+      <div className={styles.container}>
+        <h1 className={styles.title}>
+          Redirecting to{" "}
+          <a href={`https://cryptocodeschool.in/tezos/cryptobot/${id}`}>
+            Cryptobot #{id}
+          </a>
+        </h1>
+        <img src={image} className={styles.image} />
+      </div>
     </>
   );
 };
+
+function sanitizeJsonUri(origin) {
+  if (origin.startsWith("ipfs://")) {
+    return `https://cloudflare-ipfs.com/ipfs/${origin.substring(7)}/`;
+  }
+  return null;
+}
 
 export const getStaticProps = async (context) => {
   const res = await fetch(
@@ -56,13 +71,6 @@ export const getStaticProps = async (context) => {
     },
   };
 };
-
-function sanitizeJsonUri(origin) {
-  if (origin.startsWith("ipfs://")) {
-    return `https://cloudflare-ipfs.com/ipfs/${origin.substring(7)}/`;
-  }
-  return null;
-}
 
 export const getStaticPaths = async () => {
   const res = await fetch(
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,57 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useEffect } from "react";
 
-export default function Home({}) {
+export default function Home() {
+  useEffect(() => {
+    window.location.href = `https://cryptocodeschool.in/tezos/marketplace`;
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
         <title>share.cryptocodeschool.in</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.svg" />
+        <meta name="title" content={`Cryptoverse Wars | cryptocodeschool.in`} />
+        <meta
+          name="description"
+          content="Learn to code on the Tezos blockchain, the fun way"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://cryptocodeschool.in/tezos" />
+        <meta
+          property="og:title"
+          content={`Cryptoverse Wars | cryptocodeschool.in`}
+        />
+        <meta
+          property="og:description"
+          content="Learn to code on the Tezos blockchain, the fun way"
+        />
+        <meta property="og:image" content="/cover.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://cryptocodeschool.in/tezos/"
+        />
+        <meta
+          property="twitter:title"
+          content={`Cryptoverse Wars | cryptocodeschool.in`}
+        />
+        <meta
+          property="twitter:description"
+          content="Learn to code on the Tezos blockchain, the fun way"
+        />
+        <meta property="twitter:image" content="/cover.png" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Redirecting </h1>
+        <h1 className={styles.title}>
+          Redirecting to <br />
+          <a href="https://cryptocodeschool.in/tezos/marketplace">
+            Cryptoverse Wars
+          </a>{" "}
+        </h1>
       </main>
     </div>
   );
